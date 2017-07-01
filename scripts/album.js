e94d580032fab1878b6fecaed10245142f997f38
Hiroyuki Ishikawa <ishikawa.hiroyuki1118@gmail.com>
2017-07-01 17:30:51 +0900

    Assignment 11 completed

---
+++ scripts/album.js
@@ -0,0 +1,89 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/09.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumTaylor1989 = {
+     title: '1989',
+     artist: 'Taylor Swift',
+     label: 'Soundtunes',
+     year: '2016',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Welcome to New York', duration: '2:51' },
+         { title: 'Blank Space', duration: '5:01' },
+         { title: 'I Know Places', duration: '3:21'},
+         { title: 'I Wish You Would', duration: '3:14' },
+         { title: 'Out Of the Woods', duration: '2:15'},
+         { title: 'All You Had To Do Was Stay', duration: '2:25'}
+     ]
+ };
+
+var albums = [albumPicasso, albumMarconi, albumTaylor1989];
+var albumNumber = 0;
+
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+
+     return template;
+};
+
+var setCurrentAlbum = function(album) {
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
+
+    albumSongList.innerHTML = '';
+
+    for (var i = 0; i < album.songs.length; i++) {
+        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    }
+};
+
+window.onload = function() {
+    setCurrentAlbum(albums[0]);
+};
+
+document.getElementsByClassName('album-cover-art')[0].addEventListener("click", function(event) {
+    albumNumber++;
+    if(albumNumber === albums.length){albumNumber = 0;};
+    setCurrentAlbum(albums[albumNumber]);
+});
